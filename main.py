afea21ca186296d605013a5f4cd610e03b0ec675
Nikolay Gerasimov <ngerasimov@luxoft.com>
2021-04-21 16:31:50 +0300

    name

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN,
-                          webhook_url="https://mastermindbotyl.herokuapp.com/" + TOKEN)
+                          webhook_url="https://mastermind-tg-py.herokuapp.com/" + TOKEN)
     updater.idle()
 
 
